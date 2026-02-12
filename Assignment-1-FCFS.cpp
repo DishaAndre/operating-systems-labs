99f7d686023d8112ee497bc9f1c607f9f2aebde5
Disha B. Andre <andredisha55@gmail.com>
2026-02-12 10:43:29 +0530

    Refactor FCFS class to use Process struct
    Refactor FCFS scheduling implementation to use a Process array instead of separate arrays for PID and arrival time. Update input, sort, display, and calculation methods accordingly.

--- Assignment-1-FCFS.cpp
+++ Assignment-1-FCFS.cpp
@@ -1,53 +1,95 @@
 #include<iostream>
 using namespace std;
 
+struct Process {
+    string pid;      
+    int at;          
+    int bt;       
+    int ct;          
+    int tat;        
+    int wt;          
+};
 
-class FCFS{
-public:
-    string pid[100]; 
-    int at[100];  
-    int n; 
-
-    void input(){
+void input(Process p[], int &n) {
     cout << "Enter number of processes: ";
     cin >> n;
-    for (int i = 0; i < n; i++){
-        cout << "Enter Process ID for process " << i+1 << ": ";
-        cin >> pid[i];
-        cout << "Enter Arrival Time for process " << pid[i] << ": ";
-        cin >> at[i];
-        }
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Process ID: ";
+        cin >> p[i].pid;
+        cout << "Enter Arrival Time: ";
+        cin >> p[i].at;
+        cout << "Enter Burst Time: ";
+        cin >> p[i].bt;
     }
+}
 
-    void sort(){
-        for (int i = 0; i < n-1; i++){
-            for (int j = 0; j < n-i-1; j++){
-                if (at[j] > at[j+1] || (at[j] == at[j+1] && pid[j] > pid[j+1])){
-
-                    int tempAt = at[j];
-                    at[j] = at[j+1];
-                    at[j+1] = tempAt;
-                    
-                    string tempPid = pid[j];
-                    pid[j] = pid[j+1];
-                    pid[j+1] = tempPid;
-                }
+void sort(Process p[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (p[j].at > p[j+1].at) {
+                swap(p[j], p[j+1]);
             }
         }
     }
+}
 
-    void display(){
-        cout << "\nPID\tAT\n";
-        for (int i = 0; i < n; i++){
-            cout << pid[i] << "\t" << at[i] << "\n";
-        }
+void display(Process p[], int n, float avgTAT, float avgWT) {
+    cout << "\nPID\tArrival\tBurst\tCT\tTAT\tWT\n";
+    for (int i = 0; i < n; i++) {
+        cout << p[i].pid << "\t" << p[i].at << "\t" << p[i].bt << "\t" << p[i].ct << "\t" << p[i].tat << "\t" << p[i].wt << "\n";
     }
-};
+    cout << endl;
+    cout << "Avg TAT: " << avgTAT << "\n";
+    cout << "Avg WT: " << avgWT << "\n";
+}
+
+void calculate(Process p[], int n) {
+    int currentTime = 0;
+    float totalTAT = 0, totalWT = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (currentTime < p[i].at) {
+            currentTime = p[i].at;
+        }
+
+        currentTime += p[i].bt;
+        p[i].ct = currentTime;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+
+        totalTAT += p[i].tat;
+        totalWT += p[i].wt;
+    }
+
+    float avgTAT = totalTAT / n;
+    float avgWT = totalWT / n;
+
+    display(p, n, avgTAT, avgWT);
+
+    cout << "\nGantt Chart:" << endl;
+
+    cout << "P_ID :      ";
+    for (int i = 0; i < n; i++) {
+        cout << p[i].pid;
+        if (i != n - 1)
+            cout << " -> ";
+    }
+
+    cout << "\nCT   : 0";
+    for (int i = 0; i < n; i++) {
+        cout << " -> " << p[i].ct;
+    }
+
+}
+
 
 int main() {
-    FCFS p;
-    p.input();
-    p.sort();
-    p.display();
+    Process p[100];
+    int n;
+    input(p, n);
+    sort(p, n);
+    calculate(p, n);
     return 0;
 }
